40f37a4690d91222c6b8a1f56f417486f349887f
dhanashreephuge <dhanashreephuge18@gmail.com>
2025-09-09 14:31:49 +0530

    frontend code

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food</h2>
+            <p>Fast delivery | Our mission is to satisfy your cravings.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
